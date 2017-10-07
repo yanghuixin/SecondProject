d366fe65cf8069bcc1dc9599f5381be97e1b4038
yanghuixin <18237581072@163.com>
2017-10-07 22:41:40 +0800

    备份代码

--- AsyncHttpClient/src/main/java/com/yhx/asynchttpclient/MainActivity.java
+++ AsyncHttpClient/src/main/java/com/yhx/asynchttpclient/MainActivity.java
@@ -1,7 +1,17 @@
 package com.yhx.asynchttpclient;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void get_click(View view) {
+        //获取用户输入的账号密码
+        EditText et_name = (EditText) findViewById(R.id.et_name);
+        EditText et_pass = (EditText) findViewById(R.id.et_pass);
+
+        String name = et_name.getText().toString();
+        final String pass = et_pass.getText().toString();
+
+        final String path = "http://192.168.2.127:8080/Web/servlet/Login";
+
+        //使用异步HttpClient发送get请求
+        AsyncHttpClient client = new AsyncHttpClient();
+        //把要提交的数据封装到rp对象中
+        RequestParams rp = new RequestParams();
+        rp.add("name",name);
+        rp.add("pass",pass);
+        //发送get请求
+        client.get(path,rp,new MyHandler());
+    }
+
+    public void post_click(View view) {
+        //获取用户输入的账号密码
+        EditText et_name = (EditText) findViewById(R.id.et_name);
+        EditText et_pass = (EditText) findViewById(R.id.et_pass);
+
+        String name = et_name.getText().toString();
+        String pass = et_pass.getText().toString();
+
+        String path = "http://192.168.2.127:8080/Web/servlet/Login";
+        //使用异步HttpClient发送post请求
+        AsyncHttpClient client = new AsyncHttpClient();
+        //把要提交的数据封装到rp对象中
+        RequestParams rp = new RequestParams();
+        rp.add("name",name);
+        rp.add("pass",pass);
+        //发送post请求
+        client.post(path,rp,new MyHandler());
+    }
+
+    class MyHandler extends AsyncHttpResponseHandler{
+
+        //请求成功时调用
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+            Toast.makeText(MainActivity.this,new String(responseBody),Toast.LENGTH_LONG).show();
+        }
+
+        //请求失败时调用
+        @Override
+        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+            Toast.makeText(MainActivity.this,new String(responseBody),Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ AsyncHttpClient/src/main/java/com/yhx/asynchttpclient/Tools/Tools.java
@@ -0,0 +1,29 @@
+package com.yhx.asynchttpclient.Tools;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by yhx on 2017/10/3.
+ */
+
+public class Tools {
+
+    public static String getTextFromStream(InputStream is){
+        byte[] b = new byte[1024];
+        int len;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            while ((len = is.read(b)) != -1){
+                bos.write(b,0,len);
+            }
+            //把字符数组输出流转换成字节数组，然后用字节数组构造一个字符串
+            String text = new String(bos.toByteArray(),"utf-8");
+            return text;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
